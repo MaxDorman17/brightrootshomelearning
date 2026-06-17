cee0bda23c9ae5e9a8b4d3b370ccd9bd5e74b69a
Max <maxdorman17@outlook.com>
2026-06-17 22:19:30 +0100

    Update Oak test to explore NEXT_DATA

--- test_oak.py
+++ test_oak.py
@@ -2,14 +2,17 @@ import httpx, re, json
 
 url = "https://www.thenational.academy/pupils/programmes/geography-secondary-year-7/units/weather-and-climate-why-does-our-weather-and-climate-change/lessons"
 r = httpx.get(url, follow_redirects=True)
-print("status:", r.status_code)
 
-slugs = re.findall(r'/lessons/([a-z0-9-]+)"', r.text)
-print("lesson slugs found:", slugs[:6])
-print("has NEXT_DATA:", "__NEXT_DATA__" in r.text)
+m = re.search(r'<script id="__NEXT_DATA__" type="application/json">(.*?)</script>', r.text)
+data = json.loads(m.group(1))
+browse = data["props"]["pageProps"]["browseData"]
 
-if "__NEXT_DATA__" in r.text:
-    m = re.search(r'<script id="__NEXT_DATA__" type="application/json">(.*?)</script>', r.text)
-    if m:
-        data = json.loads(m.group(1))
-        print("NEXT_DATA keys:", list(data.get("props", {}).get("pageProps", {}).keys())[:10])
+# Print structure
+if isinstance(browse, list):
+    print("browseData is a list of", len(browse), "items")
+    print("first item keys:", list(browse[0].keys()) if browse else "empty")
+    for item in browse[:3]:
+        print(" ", item)
+elif isinstance(browse, dict):
+    print("browseData keys:", list(browse.keys()))
+    print(json.dumps(browse, indent=2)[:1000])
